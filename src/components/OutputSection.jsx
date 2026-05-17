bc37ee8b15beadd4b05107c953ebc117916b5c0e
Matin <misaxanli@gmail.com>
2026-05-17 06:52:52 +0200

    refactored component: removed unused microphone button and speech handler

--- src/components/OutputSection.jsx
+++ src/components/OutputSection.jsx
@@ -1,21 +1,20 @@
 import { BarLoader } from "react-spinners";
-import { FaMicrophone } from "react-icons/fa";
 
-const OutputSection = ({ translated, loading, error, handleSpeech, recognizedText, isListening }) => {
+const OutputSection = ({ translated, loading, error, recognizedText, isListening }) => {
   return (
     <div className="flex flex-col items-center justify-center h-full text-white p-4 overflow-x-hidden">
       {recognizedText && <div className=" text-xl whitespace-pre-wrap break-words max-w-full">Recognized: {recognizedText}</div>}
       {loading ? (
         <BarLoader />
       ) : error ? (
-        <div>{error}</div>
-      ) : (
+        <span className="text-red-500">{error}</span>
+      ) : translated? (
         <div className="mt-4 text-xl whitespace-pre-wrap break-words max-w-full">Translate: {translated}</div>
-      )}
+      ):(
+        <div className="mt-4 text-xl whitespace-pre-wrap break-words max-w-full">Translation will appear here</div>
+      ) 
+      }
       {isListening && <div className="text-blue-600">Listening...</div>}
-      <button className="mt-4 bg-blue-500 p-2 rounded-full hover:bg-blue-700" onClick={handleSpeech}>
-        <FaMicrophone size={24} />
-      </button>
     </div>
   );
 };
